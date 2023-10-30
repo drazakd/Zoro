4100cab90cde2ab3db88c18fba2f6aa8e59db8e5
drazakd <145049547+drazakd@users.noreply.github.com>
2023-10-30 12:45:32 +0000

    Add files via upload

---
+++ appbd.py
@@ -0,0 +1,9 @@
+DROP  TABLE IF EXISTS Produit 
+CREATE TABLE Produit(
+	CodeProduit int primary key,
+	Nom VARCHAR(50)not null,
+	Descriptions VARCHAR(200),
+	StockActuel INTEGER not null,
+	PrixUnitaire FLOAT not null,
+	CodeCategorie int not null,
+);
